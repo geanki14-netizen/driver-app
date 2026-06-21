cbdacd1310c221ba2e461117e7e3c07cef72177c
Geancarlos Grau <geanki14@gmail.com>
2026-06-21 14:09:48 -0500

    feat: conectar login/logout con AuthService y splash screen con redireccion automatica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,25 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
-    pathMatch: 'full',
+    redirectTo: 'splash',
+    pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
+    path: 'splash',
     loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
+      import('./features/splash/pages/splash/splash.page')
+        .then(m => m.SplashPage),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module')
+        .then(m => m.AuthModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module')
+        .then(m => m.HomeModule),
   },
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from './app/core/services/auth.service';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -10,5 +14,20 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(
+      withInterceptors([
+        (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+          const auth = inject(AuthService);
+          return auth.getToken().then(token => {
+            if (token) {
+              req = req.clone({
+                setHeaders: { Authorization: `Bearer ${token}` }
+              });
+            }
+            return next(req).toPromise();
+          }) as any;
+        }
+      ])
+    ),
   ],
 });
