907ee212d07edb66cd9111926c3b3df8519561e1
Geancarlos Grau <geanki14@gmail.com>
2026-06-21 14:10:11 -0500

    feat: configurar splash screen nativo con capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'DriverApp',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
